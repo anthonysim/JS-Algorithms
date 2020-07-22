51c84fa4d84eef9ffe5fa36343daf6a6c547c28a
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2020-07-21 23:25:41 -0700

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -1,27 +1,35 @@
-function binarySearch(list, item){
-  let low = 0
-  let high = list.length - 1
-
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2)
+// function takes a list of arrays and the item to search for as arguments.
+function binary_Search(list, item){
+  // established the far left end and right end of the array.
+  let left = 0
+  let right = list.length - 1
+  
+  // keeps running until item is found while left or right is shrinking in half.
+  while (left <= right) {
+    // takes the left and right end divided by 2 to come up with the midpoint.
+    // the mid position is used to "guess."
+    let mid = Math.floor((left + right) / 2)
     guess = list[mid]
 
+    // if the guess is right, it will return the position
     if (guess === item) {
       return mid
     }
-
+    
+    // if the guess is higher than the item, it will subtract 1 from mid and estabish the new right.
     if (guess > item) {
-      high = mid - 1
+      right = mid - 1
     }
-
+    
+    // if the guess is lower than the item, it will subtract 1 from mid and estabish the new left.
     else {
-      low = mid + 1
+      left = mid + 1
     }
   }
-
+  // return message if nothing is found. 
   return "No numbers found!"
 }
 
 
 console.log(binarySearch([1, 3, 5, 7, 9], 3))
-console.log(binarySearch([1, 3, 5, 7, 9], 10))
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
