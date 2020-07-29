a514f43ec38d58287ee6bb069e8104bff7ee4392
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2020-07-28 21:40:56 -0700

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -1,9 +1,9 @@
 function selectionSort(arr) { 
         
-    for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
 
         let min = i;
-        for(let j = 1; j < arr.length; j++){
+        for (let j = 1; j < arr.length; j++){
             if(arr[j] < arr[min]) {
                 min = j; 
             }
