0cf3f4443849b14147eed1b3253e2a11ffd670c9
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2020-08-07 19:49:04 -0700

    Create quickSort.js

---
+++ quickSort.js
@@ -0,0 +1,19 @@
+function quickSort(array) {
+  if (array.length < 2) {
+    return array;
+  }
+
+  var pivot = array[0];
+  var lesser = [];
+  var greater = [];
+
+  for(var i = 1; i < array.length; i++) {
+    if(array[i] < pivot) {
+      lesser.push(array[i]);
+    } else {
+      greater.push(array[i]);
+    }
+  }
+
+  return quicksortBasic(lesser).concat(pivot, quicksortBasic(greater));
+}
