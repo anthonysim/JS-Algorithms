3f7c9076c051790f30394e45f126f6cd80905c1e
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2020-11-29 13:51:39 -0800

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -1,6 +1,6 @@
 const insertionSort = function(arr) {
   for (let i = 0; i < arr.length; i++) {
-    let temp = arr[i]
+    let temp = arr[i];
     let position = i - 1;
     
     while (position >= 0) {
@@ -14,8 +14,8 @@ const insertionSort = function(arr) {
        arr[position + 1] = temp;
     }
   }
-  return arr
+  return arr;
 }
 
 
-console.log(insertionSort([5, 4, 3, 2, 1]))
+console.log(insertionSort([5, 4, 3, 2, 1]));
