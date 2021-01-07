45bc49d2d6d2bb0c9e56bea440c9e77b69478ed6
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2021-01-07 09:40:58 -0800

    Create permutation.js

---
+++ permutation.js
@@ -0,0 +1,36 @@
+// permutations using Heap Algorithm.
+
+let permutates = arr => {
+
+  const output = [];
+
+  const swapInPlace = (arr, indexA, indexB) => {
+    [arr[indexA], arr[indexB]] = [arr[indexB], arr[indexA]]
+    return arr
+  };
+
+  const generate = (n, heapArr) => {
+    if (n === 1) {
+      output.push(heapArr.join(''));
+      return;
+    }
+
+    generate(n - 1, heapArr);
+
+    for (let i = 0; i < n - 1; i++) {
+      if (n % 2 === 0) {
+        swapInPlace(heapArr, i, n - 1);
+      } else {
+        swapInPlace(heapArr, 0, n - 1);
+      }
+
+      generate(n - 1, heapArr);
+    }
+  };
+
+  generate(arr.length, arr.split(''));
+
+  return output;
+};
+
+console.log(permutates('abc'))
