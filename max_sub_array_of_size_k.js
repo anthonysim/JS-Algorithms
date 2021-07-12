95d81fc06c8efad8da64ecd4a4463dae93ffd0b7
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2021-07-11 22:06:12 -0700

    Update and rename maxSubarraySum.js to max_sub_array_of_size_k.js

---
+++ max_sub_array_of_size_k.js
@@ -0,0 +1,23 @@
+const max_sub_array_of_size_k = function(k, arr) {
+  if (arr.length < k) {
+    return null;
+  }
+  let currentTotal = 0;
+  let max = 0; 
+  let point = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    currentTotal += arr[i];
+
+    if (i >= k - 1) {
+      max = Math.max(max, currentTotal);
+      console.log(max)
+      currentTotal -= arr[point];
+      point += 1;
+    }
+  }
+  return max;
+};
+
+console.log(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])); // 9
+console.log(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])); // 7
