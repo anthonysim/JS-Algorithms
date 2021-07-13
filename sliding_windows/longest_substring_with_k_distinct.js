f39bda83299b261cbb7b8baf676d31b194238602
Anthony Sim <31682285+anthonysim@users.noreply.github.com>
2021-07-13 10:33:30 -0700

    Update longest_substring_with_k_distinct.js

--- sliding_windows/longest_substring_with_k_distinct.js
+++ sliding_windows/longest_substring_with_k_distinct.js
@@ -2,24 +2,25 @@ const longest_substring_with_k_distinct = (str, k) => {
   let charFreq = {};
   let point = 0;
   let longest = 0;
-  
+
   for (let i = 0; i < str.length; i++) {
-    let currentChar = str[i];
+    let right = str[i];
 
-    if (!(currentChar in charFreq)) {
-      charFreq[currentChar] = 0;
+    if (!(right in charFreq)) {
+      charFreq[right] = 0;
     }
-    charFreq[currentChar] += 1;
+    charFreq[right] += 1;
 
-    while(Object.keys(charFreq).length > k) {
-      charFreq[currentChar] -= 1;
+    while (Object.keys(charFreq).length > k) {
+      let left = str[point];
+      charFreq[left] -= 1;
 
-      if (charFreq[currentChar] === 0) {
-        delete charFreq[currentChar];
+      if (charFreq[left] === 0) {
+        delete charFreq[left];
       }
       point += 1;
     }
-   longest = Math.max(longest, i - point + 1);
+    longest = Math.max(longest, i - point + 1);
   }
   return longest;
 };
